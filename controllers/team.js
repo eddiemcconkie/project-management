c9a672d63f18b212f2cfa8ec3df62082658a6238
Eddie McConkie <edwardmcconkie@gmail.com>
2022-06-30 10:13:18 -0600

    Cleaned up addProjectToTeam

--- controllers/team.js
+++ controllers/team.js
@@ -53,60 +53,31 @@ exports.updateTeam = async (req, res) => {
 
 /** @type {RequestHandler} */
 exports.addProjectToTeam = async (req, res) => {
-  const user = await getUser(req)
-  if (!user) {
-    return res.status(401).json({ message: "You're not signed in!" })
+  const { teamId } = req.params
+
+  // Validate the project data
+  if (!req.body?.title || !req.body?.description) {
+    return res.status(401).json({
+      message: 'Invalid Project format. Please provide a title and description',
+    })
   }
 
   try {
-    // Get the team the user wants to add the project to
-    const { teamId } = req.params
-    const team = await Team.findById(teamId).populate('members')
+    const project = await Project.create({
+      title: req.body.title,
+      description: req.body.description,
+    })
 
-    // Make sure the user is a member of the team
-    if (team.members.findIndex((u) => u.email === user.email) >= 0) {
-      // Validate the project data
-      if (!req.body?.title || !req.body?.description) {
-        return res.status(401).json({
-          message:
-            'Invalid Project format. Please provide a title and description',
-        })
-      }
+    // If the project was created successfully, add the project ID to the team
+    await Team.findByIdAndUpdate(teamId, {
+      $push: { projects: project._id },
+    })
 
-      // Create the project
-      const projectBody = {
-        title: req.body.title,
-        description: req.body.description,
-      }
-
-      const project = new Project(projectBody)
-      const saveResult = await project.save().catch(() => null)
-      if (!saveResult) {
-        return res
-          .status(500)
-          .json({ message: 'Could not add project to database' })
-      }
-
-      // If the project was created successfully, add the project ID to the team
-      const updateResult = await Team.findByIdAndUpdate(teamId, {
-        $push: { projects: saveResult._id },
-      }).catch(() => null)
-      if (!updateResult) {
-        return res
-          .status(500)
-          .json({ message: 'Could not add project to team' })
-      }
-
-      return res.status(201).json({
-        message: 'Project added!',
-        project: formatId(project.toObject()),
-      })
-    } else {
-      // If user is not a member of the team,
-      throw new Error()
-    }
+    return res.status(201).json({
+      message: 'Project added!',
+      project: formatId(project.toObject()),
+    })
   } catch (error) {
-    console.log(error)
-    return res.status(404).json({ message: 'No team found with that ID' })
+    return res.status(500).json({ message: 'Could not add project to team' })
   }
 }
