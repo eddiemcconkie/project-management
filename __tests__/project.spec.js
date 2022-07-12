0b867ab199360e21f594dc47858aaae793e63137
jaredsanderson <jaredsa@byui.edu>
2022-07-12 17:59:22 -0600

    Project Test Works

--- __tests__/project.spec.js
+++ __tests__/project.spec.js
@@ -1,38 +1,42 @@
-// const mockingoose = require("mockingoose");
-// const Project = require("../models/project");
-// const { retriveOne } = require("./project");
+const mockingoose = require("mockingoose");
+const Project = require("../models/project");
+const {
+  Types: { ObjectId },
+} = require("mongoose");
 
-// describe("retriveOne", () => {
-//   it("should return a project", async () => {
-//     mockingoose(Project).toReturn(
-//       {
-//         _id: "62bc64dcff745674c4436493",
-//         title: "Project 1",
-//         description: "First real project",
-//         tasks: [],
-//         // createdAt: 2022-06-29T14:42:36.904+00:00
-//       },
-//       "findOne"
-//     );
-//     const results = await retriveOne("62bc64dcff745674c4436493");
-//     expect(results).toBe("test");
-//   });
-// });
+const { retrieveOne } = require("../controllers/project");
+const createdDate = new Date("2022-06-29T14:42:36.904+00:00");
 
-// // const request = require("supertest");
+jest.setTimeout(60000);
+describe("retrieveOne", () => {
+  test("Get project", async () => {
+    const _doc = {
+      _id: ObjectId("62bc64dcff745674c4436493"),
+      title: "Project 1",
+      description: "First real project",
+      tasks: [],
+      createdAt: createdDate,
+    };
 
-// // describe("Project Test Suite", () => {
-// //   it("test get /:projectid", async () => {
-// //     const response = await request(
-// //       "https://cse341-project-management.herokuapp.com"
-// //     ).get("/projects/62bc64dcff745674c4436493");
-// //     expect(response.body).toEqual({
-// //       _id: "62bc64dcff745674c4436493",
-// //       title: "Project 1",
-// //       description: "First real project",
-// //       tasks: [],
-// //       createdAt: "2022-06-29T14:42:36.904+00:00",
-// //     });
-// //     expect(response.statusCode).toBe(200);
-// //   });
-// // });
+    mockingoose(Project).toReturn(_doc, "findOne");
+
+    const req = {
+      params: { projectId: "62bc64dcff745674c4436493" },
+    };
+
+    const res = {
+      statusCode: 0,
+      status(code) {
+        this.statusCode = code;
+        return this;
+      },
+      data: {},
+      json(data) {
+        this.data = data;
+      },
+    };
+
+    await retrieveOne(req, res);
+    expect(res.data).toEqual(_doc);
+  });
+});
