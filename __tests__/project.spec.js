8398f1c32c91e9861805cf4bf4c190120854089b
jaredsanderson <jaredsa@byui.edu>
2022-07-15 22:41:49 -0600

    Project Tests Updated

--- __tests__/project.spec.js
+++ __tests__/project.spec.js
@@ -1,48 +1,16 @@
-// const mockingoose = require('mockingoose')
-// const Project = require('../models/project')
-// const {
-//   Types: { ObjectId },
-// } = require('mongoose')
-
-// const { retrieveOne } = require('../controllers/project')
-// const TestResponse = require('../lib/test-response')
-// const createdDate = new Date('2022-06-29T14:42:36.904+00:00')
-
-// jest.setTimeout(60000)
-// describe('Project routes', () => {
-//   test('Get one project', async () => {
-//     const _doc = {
-//       _id: ObjectId('62bc64dcff745674c4436493'),
-//       title: 'Project 1',
-//       description: 'First real project',
-//       tasks: [],
-//       createdAt: createdDate,
-//     }
-
-//     mockingoose(Project).toReturn(_doc, 'findOne')
-
-//     const req = {
-//       params: { projectId: '62bc64dcff745674c4436493' },
-//     }
-
-//     const res = new TestResponse()
-
-//     await retrieveOne(req, res)
-//     expect(res.statusCode).toEqual(200)
-//     expect(res.data).toEqual(_doc)
-//   })
-// })
-
 const {
   Types: { ObjectId },
 } = require("mongoose");
 
 const MockModel = require("jest-mongoose-mock");
 jest.mock("../models/project", () => new MockModel());
+jest.mock("../models/task", () => new MockModel());
 const Project = require("../models/project");
+const Task = require("../models/task");
 const projectController = require("../controllers/project");
 const TestResponse = require("../lib/test-response");
 const TestDocument = require("../lib/test-document");
+const createdDate = new Date("2022-06-29T14:42:36.904+00:00");
 
 describe("Project Routes", () => {
   let res;
@@ -52,12 +20,81 @@ describe("Project Routes", () => {
   });
 
   test("Get one project", async () => {
-    const teamDoc = new TestDocument({
-      _id: ObjectId('62bc64dcff745674c4436493'),
-      title: 'Project 1',
-      description: 'First real project',
+    const projectDoc = new TestDocument({
+      _id: ObjectId("62bc64dcff745674c4436493"),
+      title: "Project 1",
+      description: "First real project",
       tasks: [],
-      createdAt: 
+      createdAt: createdDate,
     });
+    Project.findById.mockReturnValue(projectDoc);
+
+    const req = {
+      params: { projectId: "62bc64dcff745674c4436493" },
+    };
+
+    const res = new TestResponse();
+
+    await projectController.retrieveOne(req, res);
+
+    expect(res.statusCode).toBe(200);
+    expect(res.data).toEqual(projectDoc);
+    expect(Project.findById.mock.calls.length).toBe(1);
+  });
+
+  test("Add task to project", async () => {
+    const taskDoc = new TestDocument({
+      _id: ObjectId("62d23253a71b4b3f21abfc34"),
+      title: "Awesome Task",
+      description: "Awesome Description",
+      dueDate: "2022-07-21T00:00:00.000+00:00",
+      completed: false,
+      createdAt: "2022-07-16T03:36:51.508+00:00",
+    });
+    Task.create.mockReturnValue(taskDoc);
+
+    const req = {
+      params: {
+        projectId: "62bc64dcff745674c4436493",
+      },
+      body: {
+        title: "Awesome Task",
+        description: "Awesome Description",
+        completed: false,
+      },
+    };
+
+    await projectController.addTaskToProject(req, res);
+
+    expect(res.statusCode).toBe(201);
+    expect(res.data).toEqual(taskDoc);
+    expect(Task.create.mock.calls.length).toBe(1);
+    expect(Project.findByIdAndUpdate.mock.calls.length).toBe(1);
+  });
+
+  test("Update project", async () => {
+    const projectDoc = new TestDocument({
+      _id: ObjectId("62d19dc6abcd9783e2a2befd"),
+      title: "Project 6",
+      description: "The Best Project",
+      tasks: [],
+      createdAt: "2022-07-15T17:03:02.272+00:00",
+    });
+    Project.findByIdAndUpdate.mockReturnValue(projectDoc);
+
+    const req = {
+      params: {
+        projectId: "62d19dc6abcd9783e2a2befd",
+      },
+
+      body: {
+        title: "project 6",
+        description: "the best",
+      },
+    };
+
+    await projectController.updateProject(req, res);
+    expect(res.statusCode).toBe(204);
+    expect(Project.findByIdAndUpdate.mock.calls.length).toBe(1);
   });
 });
--- controllers/project.js
+++ controllers/project.js
@@ -1,71 +1,71 @@
-const { formatId, getUser } = require('../lib/helpers')
-const Team = require('../models/team')
-const Project = require('../models/project')
-const Task = require('../models/task')
+const { formatId, getUser } = require("../lib/helpers");
+const Team = require("../models/team");
+const Project = require("../models/project");
+const Task = require("../models/task");
 
 /** @type {RequestHandler} */
 exports.retrieveAll = async (req, res) => {
   try {
-    const user = await getUser(req)
+    const user = await getUser(req);
 
     const teams = await Team.find({
       members: { $elemMatch: { $eq: user._id } },
-    }).populate('projects')
-    return res.status(200).json(teams.map((team) => team.projects).flat())
+    }).populate("projects");
+    return res.status(200).json(teams.map((team) => team.projects).flat());
   } catch (err) {
-    return res.status(500).json(err)
+    return res.status(500).json(err);
   }
-}
+};
 
 exports.retrieveOne = async (req, res) => {
   try {
-    const project = await Project.findById(req.params.projectId).lean()
-    return res.status(200).json(project)
+    const project = await Project.findById(req.params.projectId).lean();
+    return res.status(200).json(project);
   } catch (err) {
-    return res.status(500).json(err)
+    return res.status(500).json(err);
   }
-}
+};
 
 exports.updateProject = async (req, res) => {
-  const { projectId } = req.params
+  const { projectId } = req.params;
 
   if (!req.body?.title || !req.body?.description) {
     return res.status(400).json({
       message:
-        'Invalid Project format. Please provide a title and description.',
-    })
+        "Invalid Project format. Please provide a title and description.",
+    });
   }
 
   try {
     const project = await Project.findByIdAndUpdate(projectId, {
       $set: { title: req.body.title, description: req.body.description },
-    }).lean()
+    }).lean();
 
     return res.status(204).json({
-      message: 'Project Updated Successfully',
-      project: formatId(project),
-    })
+      message: "Project Updated Successfully",
+      project: project,
+    });
   } catch (err) {
-    return res.status(500).json({ message: 'Could not update the project' })
+    return res.status(500).json({ message: "Could not update the project" });
   }
-}
+};
 
 exports.getProjectTasks = async (req, res) => {
   try {
     const project = await Project.findById(req.params.projectId)
-      .populate('tasks')
-      .lean()
-    return res.status(200).json(project.tasks)
+      .populate("tasks")
+      .lean();
+    return res.status(200).json(project.tasks);
   } catch (err) {
-    return res.status(500).json(err)
+    return res.status(500).json(err);
   }
-}
+};
 
 exports.addTaskToProject = async (req, res) => {
   if (!req.body.title || !req.body.description) {
-    return res
-      .status(400)
-      .json({ message: 'Invalid format. Please provide title and description' })
+    return res.status(400).json({
+      message: "Invalid format. Please provide title and description",
+    });
   }
 
   try {
@@ -74,26 +74,26 @@ exports.addTaskToProject = async (req, res) => {
       description: req.body.description,
       tasks: [],
       dueDate: req.body.dueDate,
-    })
+    });
 
     await Project.findByIdAndUpdate(req.params.projectId, {
       $push: { tasks: task._id },
-    })
+    });
 
-    return res.status(201).json(formatId(task.toObject()))
+    return res.status(201).json(task.toObject());
   } catch (error) {
-    res.status(500).json({ message: 'Could not add task to project' })
+    res.status(500).json({ message: "Could not add task to project" });
   }
-}
+};
 
 exports.deleteProject = async (req, res) => {
   try {
-    const projectId = req.params.projectId
-    const project = await Project.findById(projectId)
+    const projectId = req.params.projectId;
+    const project = await Project.findById(projectId);
 
     // Delete all tasks for the project
     for (const taskId of project.tasks) {
-      await Task.findByIdAndDelete(taskId)
+      await Task.findByIdAndDelete(taskId);
     }
 
     // Remove the project from its team
@@ -102,11 +102,11 @@ exports.deleteProject = async (req, res) => {
         projects: { $elemMatch: { $eq: projectId } },
       },
       { $pull: { projects: projectId } }
-    )
+    );
 
-    await Project.findByIdAndDelete(projectId)
-    return res.status(204).json({ message: 'Project deleted successfully' })
+    await Project.findByIdAndDelete(projectId);
+    return res.status(204).json({ message: "Project deleted successfully" });
   } catch (error) {
-    return res.status(500).json({ message: 'Could not delete project' })
+    return res.status(500).json({ message: "Could not delete project" });
   }
-}
+};
